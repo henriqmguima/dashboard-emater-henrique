d6cd17fa5a96ebd1fff5e14727b217b6538e28eb
DocinhoÉBom <143923344+Docinho-Eh-Bom@users.noreply.github.com>
2025-09-22 21:30:56 -0300

    Update Aside.jsx
    - Modificado os ícones para o roteamento

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -1,30 +1,40 @@
-import React, { useState } from "react";
+import React from "react";
 import "../styles/Layout.css";
 import { Thermometer, Wind, CloudSun, Drop, PresentationChart } from "@phosphor-icons/react";
+import { useNavigate, useParams } from  "react-router-dom";
 
 export default function Aside() {
-    const [ativo, setAtivo] = useState(0);
-
-    const icones = [
-        <Thermometer size={40} />, // temperatura
+    //Deixei comentado caso decidir reverter 
+    //const [ativo, setAtivo] = useState(0);
+    //const icones = [
+        //<Thermometer size={40} />, // temperatura
         // <Wind size={40} />, // vento
-        <CloudSun size={40} />, // nuvens
-        <Drop size={40} />, // água
-        <PresentationChart size={40} /> // dados
+        //<CloudSun size={40} />, // nuvens
+        //<Drop size={40} />, // água
+        //<PresentationChart size={40} /> // dados
+    //];
 
+    const navigate = useNavigate();
+    const { nomeBairro } = useParams();
 
+    const opcoes = [
+        {icon: <Thermometer size={40} />, rota: "temperatura"},
+        {icon: <Wind size={40} />, rota: "vento"},
+        {icon: <CloudSun size={40} />, rota: "nuvens"},
+        {icon: <Drop size={40} />, rota: "agua"},
+        {icon: <PresentationChart size={40} />, rota: "dados"}
     ];
 
     return (
         <aside className="aside-emater">
             <nav>
-                {icones.map((icone, index) => (
+                {opcoes.map(({icon, rota}, index) => (
                     <div
                         key={index}
-                        className={`aside-item ${ativo === index ? "ativo" : ""}`}
-                        onClick={() => setAtivo(index)}
+                        className="aside-item"
+                        onClick={() => navigate(`/clima/${nomeBairro}/${rota}`)}
                     >
-                        <span>{icone}</span>
+                        <span>{icon}</span>
                     </div>
                 ))}
             </nav>
