6929c1de6b22197630ae7ae4c30ffb803294dba7
DocinhoÉBom <alessandrafreitaspacheco@gmail.com>
2025-10-12 19:17:50 -0300

    .

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -5,7 +5,7 @@ import "leaflet/dist/leaflet.css";
 import L from "leaflet";
 import icon from "leaflet/dist/images/marker-icon.png";
 import iconShadow from "leaflet/dist/images/marker-shadow.png";
-import "../styles/index.css";
+import "../styles/Index.css";
 import { MapPinPlus, ThermometerHot, Drop, CloudSun, ChartBar } from "@phosphor-icons/react";
 
 // Configuração do ícone padrão do Leaflet
@@ -126,7 +126,7 @@ const Index = ({ bairros }) => {
         {/* Footer */}
         <div className="footer">
           <p>Desenvolvido na disciplina de Extensão</p>
-          <img src="/ifsul.png" alt="IFSUL" />
+          <img src="/ifsul.png" alt="IFSUL" onClick={() => navigate(`api/inmet`)} />
         </div>
       </div>
     </div>
